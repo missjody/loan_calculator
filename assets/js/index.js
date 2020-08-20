2f3768b6bac926bd36ed82f14c922a7e0ef339ed
miss jody <missjodyrussell@gmail.com>
2020-08-19 21:38:32 -0600

    update styling, pseudo-coded out some steps

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,33 @@
-// Make some magic here!
+// on page load
+// listen for New or Used
+// listen for term
+// listen for credit score
+
+// listen for amount entered and 
+// alert if loan amt is greater than 65000 or less than 500
+    // using a modal maybe?
+
+    // if x.auto
+    // grab term from entry
+        // let n =
+    // grab credit score from entry
+        // let tier =
+    // grab loan amt from entry
+        // let PV = 
+    // grab loans type of x, term length of term, interest rate of credit score tier
+        // let interest = 
+
+    // i equals interest / 100 / 12     
+    // prep = (1 + i)
+    // let power_move = Math.pow(prep, n);
+    // let monthly_prep = (PV * i * power_move) / (power_move - 1);
+    // let monthly_payment = monthly_prep.toFixed(2);
+
+
+// if all selections have been made with acceptable information
+// dynamically display the estimated payment below the selections
+    // display monthly_payment 
+// listen for the status changes to dynamically update
+
+
+
