c001a27e32a3275394ba29a1bc16daf2f26b0620
miss jody <missjodyrussell@gmail.com>
2020-08-23 08:16:07 -0600

    set up loanAmt to grab number entered dynamically

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,40 +1,65 @@
+//keyup instead of keypress, so we get the number after new number has been added
+//no before
+let amt = 0;
+
+$("#loanAmt").keyup(function () {
+    amt = parseInt($("#loanAmt").val());
+    console.log("listening to loan amount:", amt);
+})
+
+console.log("do i remember jquery scoping", amt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // on page load
 // listen for New or Used
-$("#loanProduct")
-    .change(function () {
-        let str = "";
-        $("option:selected").each(function () {
-            str += $(this).val() + " ";
-            console.log("listening to loan product:" + str);
-        });
-    })
-    .change();
+// $("#loanProduct")
+//     .change(function () {
+//         let str = "";
+//         $("option:selected").each(function () {
+//             str += $(this).val() + " ";
+//             console.log("listening to loan product:" + str);
+//         });
+//     })
+//     .change();
 
 
 // listen for term
-$("#loanTerm")
-    .change(function () {
-        let str = "";
-        $("option:selected").each(function () {
-            str += $(this).val() + " ";
-            console.log("listening to loan term:" + str);
-        });
-    })
-    .change();
+// $("#loanTerm")
+//     .change(function () {
+//         let str = "";
+//         $("option:selected").each(function () {
+//             str += $(this).val() + " ";
+//             console.log("listening to loan term:" + str);
+//         });
+//     })
+//     .change();
 
 // listen for credit score
 
 
 // listen for amount entered and 
-$("#loanAmt")
-    .change(function () {
-        let str = "";
-        $("#loanAmt").val(function () {
-            str += $(this).val() + " ";
-            console.log("listening to loan amount:" + str);
-        });
-    })
-    .change();
+// $("#loanAmt")
+//     .change(function () {
+
+// $(document).on("change", "#loanAmt", function () {
+//     //loanAmt isn't a string, it's a number
+//     let num = parseInt($("#loanAmt").val());
+//     console.log("listening to loan amount:", num);
+
+// });
 
 // alert if loan amt is greater than 65000 or less than 500
 // using a modal maybe?
