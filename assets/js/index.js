d6b267d99f2fde2ee56e41e6cb647a867d75af2e
miss jody <missjodyrussell@gmail.com>
2020-08-23 09:08:35 -0600

    final round of pseudocode set up

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,8 @@ function calculate() {
     // if x.auto
 
     // listen for New or Used
+    // STILL NEED IF NO CAR IS SELECTED DISPLAY LINE
+    // "PLEASE SELECT NEW OR USED"
     let auto = "";
     $("#loanProduct option:selected").each(function () {
         auto += $(this).val() + " ";
@@ -12,6 +14,8 @@ function calculate() {
 
 
     // grab term from entry
+    // STILL NEED IF NO TERM IS SELECTED DISPLAY LINE
+    // "PLEASE SELECT TERM"
     let n = "";
     $("#loanTerm option:selected").each(function () {
         n += $(this).val() + " ";
@@ -19,51 +23,46 @@ function calculate() {
     });
 
 
+    // grab loan amt from entry
+    // STILL NEED IF NO AMOUNT IS ENTERED DISPLAY LINE
+    // OR IF AMOUNT DOES NOT FALL BETWEEN 499 AND 65001
+    // "PLEASE ENTER A VALUE FROM 500 TO 65,000"
+    let PV = parseInt($("#loanAmt").val());
+    console.log("listening to loan amount:", PV);
+
 
     // grab credit score from entry
+    // STILL NEED IF NO SCORE RANGE IS SELECTED DISPLAY LINE
+    // "PLEASE SELECT YOUR SCORE RANGE"
     let tier = "";
     $("#score option:selected").each(function () {
         tier += $(this).val() + " ";
         console.log("listening to loan term:" + tier);
     });
 
-    // grab loan amt from entry
-    let PV = parseInt($("#loanAmt").val());
-    console.log("listening to loan amount:", PV);
-
-    // STILL NEED TO ADD LIMITATIONS TO PV
-    // alert if loan amt is greater than 65000 or less than 500
-    // using a modal maybe?
-
     // STILL NEED TO SERVE ACTUAL INTEREST
     // grab loans type of x, term length of term, interest rate of credit score tier
     // let interest = 
 
-    // to test, hardcode interest
-    let interest = 5.99;
 
-    // i equals interest / 100 / 12
+    // magical math moment
     let i = interest / 100 / 12;
-    // prep = (1 + i)
     let prep = (1 + i);
     let power_move = Math.pow(prep, n);
     let monthly_prep = (PV * i * power_move) / (power_move - 1);
     let monthly_payment = monthly_prep.toFixed(2);
 
-    console.log("monthly payment test", monthly_payment)
-
+    // STILL NEED TO SET UP
     // if all selections have been made with acceptable information
     // dynamically display the estimated payment below the selections
     // display monthly_payment 
-    // listen for the status changes to dynamically update
 }
 
-// on page load
+// on page load, listen for any form changes
+// if changes are made, update calculation
 $(function () {
 
     $("form").change(calculate);
     $("form").keyup(calculate);
-
-
 
 });
