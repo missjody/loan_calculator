568096aab799e13607885cc7c47d565de3dfe827
miss jody <missjodyrussell@gmail.com>
2020-09-30 19:34:23 -0600

    api call passing to AutoCalculator component

--- components/calculator/autoCalculator.js
+++ components/calculator/autoCalculator.js
@@ -1,27 +1,18 @@
-import React from "react" // , { useState }
-import FormLayout from "./formLayout.js"
-import Head from 'next/head'
+import React, { useState } from "react" // , 
+// import FormLayout from "./formLayout.js"
+// import Head from 'next/head'
 
-
-
-// do we pull the api here? 
-// this gets called on every request
-// export async function getServerSideProps() {
-//     const response = await fetch("http://localhost:3000/api/rates")
-//     const rates = await response.json()
-//     //passes to page via props
-//     return { props: { rates } }
-// }
 
 // need to pass over props for the calculator to use -- check syntax for that
-function AutoCalculator() {
+function AutoCalculator({ rates }) {
+    // console.log("Do rates pass over? ", rates) THEY DO hurray
 
     // // what we'll be grabbing from the entries of the user
-    // const [userEntry, setUserEntry] = useState({
-    //     loanAmount: "",
-    //     scoreRange: "",
-    //     term: "",
-    // })
+    const [userEntry, setUserEntry] = useState({
+        loanAmount: "",
+        scoreRange: "",
+        term: "",
+    })
 
     // // so we can set the interest rate
     // const [userRate, setUserRate] = useState({
@@ -120,80 +111,86 @@ function AutoCalculator() {
     //     });
     // };
 
-    // return form from here
+    // // return form from here
+    // return (
+    //     <FormLayout>
+    //         <Head>
+    //             <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css" />
+    //             <title>Auto Loan Payment Calculator</title>
+    //             <link rel="icon" href="/favicon.ico" />
+    //         </Head>
+
+    //         {/* <h1 className={styles.title}>
+    //             Loan Payment Calculator
+    //         </h1> */}
+
+    //         {/* <p className={styles.description}> */}
+    //         {/* loan calculator intro */}
+    //         <h5>Calculate your estimated monthly loan payment by entering the loan information below.</h5>
+    //         {/* </p> */}
+
+    //         {/* <p className="error">{error}</p> */}
+
+    //         {/* form */}
+    //         <form >
+    //             {/* onChange={handleSubmitValues} */}
+    //             {/* do a truthy, if there is not a result yet only display the questions, if there is a result also show the result box and refresh button 
+    //             so basically double up these options once we figure out if they can stay selects or inputs */}
+    //             {/* change these to inputs? do I remember how this is done in React, or rather in Next */}
+    //             <select className="custom-select" id="loanProduct">
+    //                 <option selected>Choose your loan product</option>
+    //                 <option value="New">New Auto (2017 & newer)</option>
+    //                 <option value="Used">Used Auto (2010 - 2016)</option>
+    //             </select>
+
+    //             <select className="custom-select" id="loanTerm">
+    //                 <option selected>Choose your loan term</option>
+    //                 <option value="60">60 months</option>
+    //                 <option value="72">72 months</option>
+    //                 <option value="84">84 months</option>
+    //             </select>
+
+    //             <select className="custom-select" id="score">
+    //                 <option selected>Do you know your credit score</option>
+    //                 <option value="APlus">Excellent (740 & above)</option>
+    //                 <option value="A">Great (690-739)</option>
+    //                 <option value="B">Good (660-689)</option>
+    //                 <option value="C">Fair (620-659)</option>
+    //                 <option value="D">Ok (619 & Below)</option>
+    //             </select>
+
+    //             <input type="number" className="form-control" id="loanAmt"
+    //                 placeholder="Enter desired loan amount from $500 to $65,000">
+    //             </input>
+    //         </form>
+
+    //         <div>
+    //             <h5 id="monthlyPayment"></h5>
+    //         </div>
+    //         {/* 
+    //         <div>
+    //             <input
+    //                 className='button'
+    //                 value='Start Over'
+    //                 type='button'
+    //                 onClick={clearFields}
+    //             />
+    //         </div> */}
+
+    //     </FormLayout>
+    // )
+
     return (
-        <FormLayout>
-            <Head>
-                <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css" />
-                <title>Auto Loan Payment Calculator</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            {/* <h1 className={styles.title}>
-                Loan Payment Calculator
-            </h1> */}
-
-            {/* <p className={styles.description}> */}
-            {/* loan calculator intro */}
-            <h5>Calculate your estimated monthly loan payment by entering the loan information below.</h5>
-            {/* </p> */}
-
-            {/* <p className="error">{error}</p> */}
-
-            {/* form */}
-            <form >
-                {/* onChange={handleSubmitValues} */}
-                {/* do a truthy, if there is not a result yet only display the questions, if there is a result also show the result box and refresh button 
-                so basically double up these options once we figure out if they can stay selects or inputs */}
-                {/* change these to inputs? do I remember how this is done in React, or rather in Next */}
-                <select className="custom-select" id="loanProduct">
-                    <option selected>Choose your loan product</option>
-                    <option value="New">New Auto (2017 & newer)</option>
-                    <option value="Used">Used Auto (2010 - 2016)</option>
-                </select>
-
-                <select className="custom-select" id="loanTerm">
-                    <option selected>Choose your loan term</option>
-                    <option value="60">60 months</option>
-                    <option value="72">72 months</option>
-                    <option value="84">84 months</option>
-                </select>
-
-                <select className="custom-select" id="score">
-                    <option selected>Do you know your credit score</option>
-                    <option value="APlus">Excellent (740 & above)</option>
-                    <option value="A">Great (690-739)</option>
-                    <option value="B">Good (660-689)</option>
-                    <option value="C">Fair (620-659)</option>
-                    <option value="D">Ok (619 & Below)</option>
-                </select>
-
-                <input type="number" className="form-control" id="loanAmt"
-                    placeholder="Enter desired loan amount from $500 to $65,000">
-                </input>
-            </form>
-
-            <div>
-                <h5 id="monthlyPayment"></h5>
-            </div>
-            {/* 
-            <div>
-                <input
-                    className='button'
-                    value='Start Over'
-                    type='button'
-                    onClick={clearFields}
-                />
-            </div> */}
-
-        </FormLayout>
+        <h1> Test </h1>
     )
+
+
 }
 
 
 
 
-export default AutoCalculator()
+export default AutoCalculator
 
 
 
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
 // import Head from "next/head"
 // import styles from "../styles/Home.module.css"
-// import AutoCalculator from "../components/calculator/autoCalculator.js"
+import AutoCalculator from "../components/calculator/autoCalculator.js"
 import fetch from "isomorphic-unfetch"
 
-export default function Index() {
-    // console.log("Test")
+export default function Index({ rates }) {
+    // console.log("Test: ", rates.data.loans.New)
+    // so go down to whatever is selected, new or used {}
+    // then map through to match term length
+    // and grab rates that go with that term
+
     return (
-        // <AutoCalculator>
-        //     {/* it looks like in next this code goes here */}
-        // </AutoCalculator>
         <div>
+
             <h1>beep boop</h1>
+            <AutoCalculator rates={rates} />
+
         </div>
     )
 }
@@ -22,7 +26,7 @@ Index.getInitialProps = async function () {
     const data = await response.json();
 
     console.log("Rendering... ")
-    console.log("Data: ", data)
+    // console.log("Data: ", data.loans.New)
     return {
         rates: { data }
     }
