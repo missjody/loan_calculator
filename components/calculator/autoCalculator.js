d0cb113401b6534b1e53ea6ca80eafa3e6703066
miss jody <missjodyrussell@gmail.com>
2020-10-01 19:47:26 -0600

    slide for loan amount corrected for minmax values

--- components/calculator/autoCalculator.js
+++ components/calculator/autoCalculator.js
@@ -72,28 +72,29 @@ function AutoCalculator({ rates }) {
         // map over data to find match to value of userEntry.loanType
         // map over data to find match to value of userEntry.term
         // map over that term's rates to match to value of userEntry.scoreRange
-        // setUserRate to the rate found
+        // setUserRate(to the rate found)
     }
 
     // do the math
     // Calculation
     // set up results to the state to be displayed to the user
-    const calculate = ({ amount, interest, term }) => {
-        const userAmount = Number(amount);
-        const i = Number(interest) / 100 / 12;
-        let prep = (1 + i)
-        const months = Number(term) * 12;
-        const power_move = Math.pow(prep, months);
-        const monthly_prep = (userAmount * power_move * i) / (power_move - 1);
-        const monthly_payment = monthly_prep.toFixed(2);
+    // we'll be passing these values in differently cause it's userRate.interest and userEntry.loanAmount, userEntry.term
+    // const calculate = ({ amount, interest, term }) => {
+    //     const userAmount = Number(amount);
+    //     const i = Number(interest) / 100 / 12;
+    //     let prep = (1 + i)
+    //     const months = Number(term) * 12;
+    //     const power_move = Math.pow(prep, months);
+    //     const monthly_prep = (userAmount * power_move * i) / (power_move - 1);
+    //     const monthly_payment = monthly_prep.toFixed(2);
 
-        setResults({
-            monthlyPayment: monthly_payment,
-            isResult: true,
-        });
+    //     setResults({
+    //         monthlyPayment: monthly_payment,
+    //         isResult: true,
+    //     });
 
-        return;
-    }
+    //     return;
+    // }
 
     // set up ability to clear information and start over
     // Clear input fields
@@ -114,7 +115,6 @@ function AutoCalculator({ rates }) {
     return (
         <div>
 
-
             <h1 >
                 Loan Payment Calculator
             </h1>
@@ -122,7 +122,7 @@ function AutoCalculator({ rates }) {
             {/* loan calculator intro */}
             <h5>Calculate your estimated monthly loan payment by entering the loan information below.</h5>
 
-            <p className="error">{error}{userEntry.term}TEST</p>
+            <p className="error">{error}</p>
 
             <form>
 
@@ -133,14 +133,14 @@ function AutoCalculator({ rates }) {
                 </select>
 
 
-                <select className="custom-select" id="loanTerm">
+                <select className="custom-select" id="loanTerm" name="term" onChange={handleInputChange}>
                     <option selected>Choose your loan term</option>
                     <option value="60">60 months</option>
                     <option value="72">72 months</option>
                     <option value="84">84 months</option>
                 </select>
 
-                <select className="custom-select" id="score">
+                <select className="custom-select" id="score" name="scoreRange" onChange={handleInputChange}>
                     <option selected>Do you know your credit score</option>
                     <option value="APlus">Excellent (740 & above)</option>
                     <option value="A">Great (690-739)</option>
@@ -149,8 +149,8 @@ function AutoCalculator({ rates }) {
                     <option value="D">Ok (619 & Below)</option>
                 </select>
 
-                {/* <p> I want to borrow ${userEntry.loanAmount}</p> */}
-                <input type="range" step={1} maxvalue={65000} minvalue={500} onChange={handleInputChange} />
+                <p> I want to borrow ${userEntry.loanAmount}</p>
+                <input type="range" step={500} max={65000} min={500} name="loanAmount" onChange={handleInputChange} />
 
                 {/* <input type="number" className="form-control" id="loanAmt"
                     placeholder="Enter desired loan amount from $500 to $65,000">
@@ -177,8 +177,6 @@ function AutoCalculator({ rates }) {
     )
 
 }
-
-
 
 
 export default AutoCalculator
