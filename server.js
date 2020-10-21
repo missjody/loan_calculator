9f99e03d7514a4d32b6dc52cee9f21bb105e4afa
miss jody <missjodyrussell@gmail.com>
2020-10-21 09:04:06 -0600

    error:  Error R10 (Boot timeout) -> Web process failed to bind to  within 60 seconds of launch

--- server.js
+++ server.js
@@ -29,9 +29,10 @@ const next = require("next");
 /////
 // set up Express server
 
-const port = process.env.PORT || 3000
+// const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== "production"
-const app = next({ dev })
+const app = next({dir: '.', dev })
+
 const handle = app.getRequestHandler()
 
 app.prepare()
@@ -40,17 +41,17 @@ app.prepare()
     // initiate express
     const server = express()
 
+    // changes from suggestion after getting error
+    //  Error R10 (Boot timeout) -> Web process failed to bind to $PORT within 60 seconds of launch
+    server.set('port', (process.env.PORT || 3000));
+
     // wildcard route to catch all routes, passed to getRequestHandler()
     server.get("*", (req, res) => {
       return handle(req, res)
     })
 
-    server.listen(port, (err) => {
+    server.listen(server.get('port'), (err) => {
       if (err) throw err
-      console.log("server ready, running on http://localhost:" + port + " ~(o v o.)~ ")
-    })
-
-  }).catch((ex) => {
-    console.error(ex.stack)
-    process.exit(1)
+      console.log(`> Ready on http://localhost:${server.get('port')}`)
   })
+})
